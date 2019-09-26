14d7f49f7222139749f9aaf0bb059bb848f0f49f
waivey <waivey@gmail.com>
2019-09-26 12:15:08 +0100

    changed from React page to a clock app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import FirstComponent from './FirstComponent'
-  class App extends Component {  
-    render() {    
-      return (      
-      <div className="App">        
-      <header className="App-header">          
-        <img src={logo} className="App-logo" alt="logo" />          
-        <h1 className="App-title">Welcome to React</h1>        
-      </header>        
-      <p className="App-intro">          
-        To get started, edit <code>src/App.js&lt;</code> and save to reload.        
-      </p>        
-      <FirstComponent displaytext="First Component Data"/>      
-      </div>
-    );  
-  }}
-export default App;
+  class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(), 1000
+      );
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+
+    tick() {
+      this.setState({
+        date: new Date()
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>The current time is</h1>
+          <h2>{this.state.date.toLocaleTimeString()}.</h2>
+        </div>  
+      );
+    }
+  }
+export default Clock;
